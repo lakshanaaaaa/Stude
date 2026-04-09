b5eeb79b0c405e9331a2198a9851b42e38e96fd4
Dinesh S <dineshsenathipathi@gmail.com>
2026-04-09 13:00:26 +0530

    fix: Security fix for client/src/components/ui/select.tsx

--- client/src/components/ui/select.tsx
+++ client/src/components/ui/select.tsx
@@ -15,7 +15,7 @@ const SelectValue = SelectPrimitive.Value
 const SelectTrigger = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Trigger>,
   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
+>(({ className, React.createElement('div', { dangerouslySetInnerHTML: { __html: children } }), ...props }, ref) => (
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
