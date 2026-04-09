1786fa77964f8b9aeef30c8432655dec23de32f4
Dinesh S <dineshsenathipathi@gmail.com>
2026-04-09 13:00:22 +0530

    fix: Security fix for client/src/components/ui/menubar.tsx

--- client/src/components/ui/menubar.tsx
+++ client/src/components/ui/menubar.tsx
@@ -44,7 +44,7 @@ const Menubar = React.forwardRef<
     ref={ref}
     className={cn(
       "flex h-10 items-center space-x-1 rounded-md border bg-background p-1",
-      className
+      DOMPurify.sanitize(className)
     )}
     {...props}
   />
